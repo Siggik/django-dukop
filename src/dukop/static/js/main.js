267f10a2fb4095834762d94b40f73845c8e3f9fb
Benjamin Bach <benjamin@overtag.dk>
2022-01-23 12:20:36 +0100

    Add click() event listener for input w/ feed URL

--- src/dukop/static/js/main.js
+++ src/dukop/static/js/main.js
@@ -1,14 +1,17 @@
-$("input.share-text-input").focus(function() {
+$("input.share-text-input").focus(select_all);
+$("input.share-text-input").click(select_all);
+
+function select_all() {
    $(this).select();
    $(this).attr("readonly", true);
-});
+}
 
 function copy() {
    var copyText = document.querySelector("input.share-text-input");
    copyText.select();
    document.execCommand("copy");
  }
- 
+
 if (document.querySelector(".js-copy")) {
    document.querySelector(".js-copy").addEventListener("click", copy);
 }
